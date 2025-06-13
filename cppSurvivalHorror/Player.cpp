90fb042c4f3fc1d132feac1d83492569e14a076f
Jordan Lundy <jordanlundy427@gmail.com>
2025-06-12 21:05:34 -0600

    added basic player class

---
+++ cppSurvivalHorror/Player.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+#include <string>
+
+class Player {
+public:
+	Player()
+	{
+		this->health = 100;
+		this->sanity = 100;
+	}
+
+	Player(int health, int sanity)
+	{
+		this->health = health;
+		this->sanity = sanity;
+	}
+
+	void changeHealth(int add)
+	{
+		if ((health + add) >= 100)
+		{
+			health = 100;
+		}
+		else if ((health + add) <= 0)
+		{
+			health = 0;
+		}
+		else
+		{
+			health += add;
+		}
+	}
+
+	void changeSanity(int add)
+	{
+		if ((sanity + add) >= 100)
+		{
+			sanity = 100;
+		}
+		else if ((sanity + add) <= 0)
+		{
+			sanity = 0;
+		}
+		else
+		{
+			sanity += add;
+		}
+	}
+
+	int getHealth()
+	{
+		return health;
+	}
+
+	int getSanity()
+	{
+		return sanity;
+	}
+
+	std::vector<std::string> getInventory()
+	{
+		return inventory;
+	}
+
+	void addInventory(std::vector<std::string> incoming)
+	{
+		for (int i = 0;i < incoming.size();i++)
+		{
+			inventory.push_back(incoming[i]);
+		}
+	}
+private:
+	int health;
+	int sanity;
+	std::vector<std::string> inventory;
+};
