798c9bfb96bc6040bde35638ea1f33754fdb3eac
Jordan Lundy <jordanlundy427@gmail.com>
2025-06-16 22:01:32 -0600

    added basic functionalities for enemy

--- cppSurvivalHorror/Enemy.cpp
+++ cppSurvivalHorror/Enemy.cpp
@@ -6,9 +6,67 @@ class Enemy
 public:
 	Enemy()
 	{
+		seesPlayer = false;
+		isRunning = false;
+		health = 100;
+		isStunned = false;
+	}
 
+	bool getSeesPlayer()
+	{
+		return seesPlayer;
+	}
+
+	bool getRunningStatus()
+	{
+		return isRunning;
+	}
+
+	int getHealth()
+	{
+		return health;
+	}
+
+	bool getIsStunned()
+	{
+		return isStunned;
+	}
+
+	void changeHealth(int add)
+	{
+		if ((health + add) >= 100)
+		{
+			health = 100;
+		}
+		else if ((health + add) <= 0)
+		{
+			health = 0;
+		}
+		else
+		{
+			health += add;
+		}
+	}
+
+	void playerSpottedStatus(bool status)
+	{
+		seesPlayer = status;
+	}
+
+	void runningStatus(bool status)
+	{
+		isRunning = status;
+	}
+
+	void isStunnedStatus(bool status)
+	{
+		isStunned = status;
 	}
 
 private:
+	bool seesPlayer;
+	bool isRunning;
+	int health;
+	bool isStunned;
 
 };
